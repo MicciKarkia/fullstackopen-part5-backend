2b531dd84c75fc685ed1194c85a1019f7849d9f9
Michaela Karkia <michaela@karkia.me>
2020-07-06 13:47:26 +0300

    helper functions and unit tests step2, 4.4, completed

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogs => {
+  const result = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  console.log('totalLikes result:', result)
+  return result
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
