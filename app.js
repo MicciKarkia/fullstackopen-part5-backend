29ac640e9ec1ac474ffc0c85002afb4c7508c9ed
Michaela Karkia <michaela@karkia.me>
2020-07-06 12:30:28 +0300

    blog list step2 4.2 completed

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/blogs', blogsRouter)
+app.use('/bloglist', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
