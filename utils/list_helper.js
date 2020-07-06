ebb790303902ff854070dcb9377026c085957576
Michaela Karkia <michaela@karkia.me>
2020-07-06 15:15:01 +0300

    helper functions and unit tests step3, 4.5*, completed

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,11 +4,26 @@ const dummy = blogs => {
 
 const totalLikes = blogs => {
   const result = blogs.reduce((sum, blog) => sum + blog.likes, 0)
-  console.log('totalLikes result:', result)
+  //console.log('totalLikes result:', result)
+  return result
+}
+
+const favoriteBlog = blogs => {
+  const max =
+    blogs.length === 0
+      ? 'empty list'
+      : blogs.reduce((max, blog) => (max.likes > blog.likes ? max : blog))
+
+  const result =
+    max === 'empty list'
+      ? max
+      : { title: max.title, author: max.author, likes: max.likes }
+
   return result
 }
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
