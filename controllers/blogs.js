a38fe3a45777230a04b88a85b48168ab62928ff8
MicciKarkia <micci@karkia.me>
2020-07-09 13:09:42 +0300

    blog list expansions step2, 4.14, completed

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,7 +3,6 @@ const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
-
   if (blogs) {
     response.json(blogs.map(blog => blog.toJSON()))
   } else {
@@ -11,32 +10,37 @@ blogsRouter.get('/', async (request, response) => {
   }
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.json(blog.toJSON())
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
-})
-
-blogsRouter.post('/', async (request, response, next) => {
-  const blog = new Blog(request.body)
-
-  try {
-    const savedBlog = await blog.save()
-    response.json(savedBlog.toJSON())
-  } catch (exception) {
-    next(exception)
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog.toJSON())
+  } else {
+    response.status(404).end()
   }
 })
 
-blogsRouter.delete('/:id', async (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+
+  const savedBlog = await blog.save()
+  response.json(savedBlog.toJSON())
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const blog = {
+    likes: body.likes,
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  })
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
