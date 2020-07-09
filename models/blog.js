744fb636a96e5d5727b52e276138ed601c96267d
MicciKarkia <micci@karkia.me>
2020-07-09 14:24:49 +0300

    bloglist expansion step3, 4.15, completed

--- models/blog.js
+++ models/blog.js
@@ -19,6 +19,10 @@ const blogSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Blog',
+  },
 })
 
 blogSchema.set('toJSON', {
